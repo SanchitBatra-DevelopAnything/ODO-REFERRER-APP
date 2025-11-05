3b8ed3459f47adcedd4d7b37f4b598d46868996c
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2025-11-06 02:59:45 +0530

    Removed the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,7 @@ import 'package:provider/provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-  statusBarColor: Colors.transparent, 
-  statusBarIconBrightness: Brightness.light,
-));
-
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyApp());
 }
 
